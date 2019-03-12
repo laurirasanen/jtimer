874145e7821e9342c8e4ee9a9a11b89f0907fc6c
Lauri Räsänen <pancakelarry@gmail.com>
2019-03-12 03:54:15 +0200

    Add sub-tick zone logic

--- addons/source-python/plugins/opentimer/core/zones/zone.py
+++ addons/source-python/plugins/opentimer/core/zones/zone.py
@@ -1,10 +1,14 @@
 import mathlib
 import math
+from mathlib import Vector
 
 class Zone():
     def __init__(self, zone_id, center=mathlib.NULL_VECTOR, extents=mathlib.NULL_VECTOR):
         self.center = center
-        self.extents = extents      
+        # half-widths
+        self.extents = extents
+        # opposite corners
+        self.bounds = (self.center - self.extents, self.center + self.extents)
 
     def is_overlapping(self, other_center, other_extents):
         """AABB-AABB test
@@ -14,3 +18,55 @@ class Zone():
         z = abs(self.center[2] - other_center[2]) <= (self.extents[2] + other_extents[2]);
      
         return x and y and z
+
+    def time_to_zone_edge(self, other_center, other_extents, velocity):
+        """sub-tick time until player bounding box will leave the zone"""
+        # get closest corner to zone
+        corner = self.closest_corner(other_center, other_extents)
+
+        # https://gamedev.stackexchange.com/a/18459
+        direction = Vector.normalized(velocity)
+        dirfrac = mathlib.NULL_VECTOR
+        for i in range(0, 3):
+            dirfrac[i] = 1.0 / direction[i]
+        # t1, t3, t5
+        vt0 = (self.bounds[0] - corner)*dirfrac
+        # t2, t4, t6
+        vt1 = (self.bounds[1] - corner)*dirfrac
+
+        tmin = max(max(min(vt0[0], vt1[0]), min(vt0[1], vt1[1])), min(vt0[2], vt1[2]))
+        tmax = min(min(max(vt0[0], vt1[0]), max(vt0[1], vt1[1])), max(vt0[2], vt1[2]))
+
+        # max distance travelled in a tick ( sqrt(sqrt(3500^2 + 3500^2) + 3500^2) )
+        t = 91
+
+        # if tmax < 0, ray is intersecting AABB, but the whole AABB is behind us
+        if tmax < 0:
+            t = tmax
+
+        # if tmin > tmax, ray doesn't intersect AABB
+        # this shouldn't happen if we are about to exit or enter it...
+        # this function should only be run after the fact, 
+        # using player values from the previous tick
+        if tmin > tmax:
+            t = tmax
+
+        t = tmin
+
+        # fraction of ticks to zone edge
+        time = t / velocity.length
+
+        # clamp between 0 and 1
+        return max(0, min(time, 1))
+
+    def closest_corner(self, other_center, other_extents):
+        """get closest corner of other box to ours"""
+        corner = mathlib.NULL_VECTOR
+
+        for i in range(0, 3):
+            if self.center[i] - other_center[i] > 0:
+                corner[i] = other_center[i] + other_extents[i]
+            else:
+                corner[i] = other_center[i] - other_extents[i]
+
+        return corner
