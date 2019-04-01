48ace4f74a1b133a157c751f3b9929d6c5442a0f
laurirasanen <laurirasanen@occasionally.cool>
2019-04-01 21:49:11 +0300

    Change config to .ini for comments

--- addons/source-python/plugins/jtimer/core/config.py
+++ addons/source-python/plugins/jtimer/core/config.py
@@ -1,11 +1,16 @@
-import json
+from configparser import ConfigParser
 
 from .constants.paths import CFG_PATH
 
-CORE_CFG_FILE = CFG_PATH / "core.json"
-CORE_CFG = None
+parser = ConfigParser()
 
-with CORE_CFG_FILE.open("r") as file:
-    CORE_CFG = json.load(file)
+CORE_CFG_FILE = CFG_PATH / "core.ini"
+parser.read(CORE_CFG_FILE)
+API_CFG = dict(parser.items("api"))
 
-API_CFG = CORE_CFG["api"]
+# validate stuff
+assert "host" in API_CFG
+assert "upload" in API_CFG
+if API_CFG["upload"]:
+	assert "username" in API_CFG
+	assert "password" in API_CFG
