42718cc9356fd18f24948bea325a0cb12d9ad448
laurirasanen <laurirasanen@occasionally.cool>
2019-06-25 22:09:26 +0300

    Only check python major version

--- addons/source-python/plugins/jtimer/setup.py
+++ addons/source-python/plugins/jtimer/setup.py
@@ -11,8 +11,6 @@ from pip._internal import main as pipmain
 if __name__ == "__main__":
     # make sure we're running same python version as SP
     assert sys.version_info.major == 3
-    assert sys.version_info.minor == 6
-    assert sys.version_info.micro == 1
     assert platform.architecture()[0] == "32bit"
 
     with open("requirements.txt") as requirements:
